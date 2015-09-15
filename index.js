942b68a1b1534b868ba2f0a31570acb0dcef8b56
makotot <makoto.ttn@gmail.com>
2015-09-15 12:16:00 +0900

    fix error : Cannot read property 'filter' of null

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ module.exports = function (template, cb) {
   var partialPattern = /\{\{>( )?[A-z0-9._-]*( )?\}\}/g,
     unBodyPattern = /^(?!.*body)/;
 
-  var matched = template.match(partialPattern).filter(function (partial) {
+  var matched = template.match(partialPattern);
+
+  if (!matched) {
+
+    if (typeof cb === 'function') {
+      cb(null, []);
+    }
+
+    return [];
+  }
+
+  matched = matched.filter(function (partial) {
     return unBodyPattern.test(partial);
   });
 
